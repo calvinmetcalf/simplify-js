bb24d609e0980506c52caac7d54570ebae8d28e9
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-10 21:35:49 -0400

    all but the bit shifts

--- simplify.js
+++ simplify.js
@@ -96,7 +96,7 @@ window.stuff=[];
 					f64[j+1]=pointy;
 					prevPointx = pointx;
 					prevPointy = pointy;
-					j = j + 2;
+					j = (j + 2)|0;
 				}
 			}
 			f64[j]=pointx;
@@ -111,13 +111,9 @@ window.stuff=[];
 		function simplifyDouglasPeucker(sqTolerance,nlen,hq) {
 			sqTolerance = +sqTolerance;
 			nlen = nlen|0;
-            hq=+hq;
-            var len = 0;
-            len = nlen;
-            if(hq==0.0){
-                len = simplifyRadialDistance(sqTolerance,nlen);
-            }
-			var first = 0,
+            hq= +hq;
+            var len = 0,
+            first = 0,
 			last  = 0,
 			i =0,
 			slen = 0,
@@ -131,14 +127,18 @@ window.stuff=[];
             mShift = (heap.byteLength>>3);
             fShift = ((heap.byteLength>>1)+(heap.byteLength>>2))>>2;
             lShift = ((heap.byteLength>>1)+(heap.byteLength>>3))>>2;
-			last = len -2;
+            len = nlen;
+            if(hq==0.0){
+                len = simplifyRadialDistance(sqTolerance,nlen);
+            }
+			last = (len - 2)|0;
 
 			i32[mShift+first] = i32[mShift+last] = 1;
 
 			while (last) {
 				maxSqDist = 0.0;
 
-				for (i = first + 2; i < last; i=i+2) {
+				for (i = (first + 2)|0; (i|0) < (last|0); i=(i+2)|0) {
 					sqDist = getSquareSegmentDistance(f64[i],f64[i+1], f64[first],f64[first+1], f64[last],f64[last+1]);
 	
 					if (sqDist > maxSqDist) {
@@ -152,27 +152,27 @@ window.stuff=[];
 
 					i32[fShift+slen] = first;
 					i32[lShift+slen] = index;
-					slen = slen + 1;
+					slen = (slen + 1)|0;
 					i32[fShift+slen] = index;
 					i32[lShift+slen] = last;
-					slen = slen + 1;
+					slen = (slen + 1)|0;
                     
 			}
 
 		
 			
-                      slen=slen-1;
+                      slen=(slen-1)|0;
 				    first=i32[fShift+slen];
 				     last = i32[lShift+slen]
 				
 			}
 
-			for (i = 0; i < len; i=i+2) {
+			for (i = 0; (i|0) < (len|0); i=(i+2)|0) {
 				if (i32[mShift+i]) {
 					f64[nlen+outlen] = f64[i];
-					outlen = outlen + 1;
+					outlen = (outlen + 1)|0;
 					f64[nlen+outlen] = f64[i+1];
-					outlen = outlen + 1;
+					outlen = (outlen + 1)|0;
 				}
 			}
 
@@ -185,7 +185,6 @@ window.stuff=[];
 			 ? exports
 			 : global;
 	root.simplify = function (points, tolerance, highestQuality) {
-        var zz = new Date();
 		var array = (new Float64Array(points.length*4));
 		array.set(points);
 		var buffer = array.buffer;
@@ -194,15 +193,10 @@ window.stuff=[];
 						? tolerance * tolerance
 						: 1;
 		var len=points.length;
-        zz=(new Date())-zz;
-        var tt;
-        tt= new Date();
 		var outlen = asm.simplifyDouglasPeucker(sqTolerance,len,highestQuality);
-        tt = (new Date())-tt;
-        var tz = new Date();
         var oot = new Float64Array(buffer,buffer.byteLength>>2,outlen);
-        var tz = (new Date())-tz;
-        console.log([zz,tt,tz]);
+
+ 
 		return oot;
 };
 
